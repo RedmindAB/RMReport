bf767a721305faa3e8a374efb405fdf7d463a064
SnytaxError <grimlund@gmail.comgit config --global user.name SnytaxErrorgit commit -am hello>
2015-01-26 11:23:45 +0100

    broken error message

--- src/main/resources/static/scripts/controllers/logs-controller.js
+++ src/main/resources/static/scripts/controllers/logs-controller.js
@@ -3,13 +3,33 @@ angular.module('webLog')
     $scope.message = "Logs Controller";
     $scope.errorReport={};
     $scope.log = {};
+    $scope.display ={};
     $http.get('/api/log/getloglist')
     .success(function(data, status, headers, config){ 
     	if(data){
     		$scope.log = data;
-    		console.log(data);
+    		console.log($scope.log);
+
     	}
     }).error(function(data, status, headers, config){
     	console.log(data);
     });
+    $scope.populateLog=function(){
+    getTestSuite("CreateLogTest", "(20150121-160711)");
+    }
+
+    var getTestSuite = function(suiteName, timeStamp){
+            var suiteToReturn = {};
+
+            for(var suite in $scope.log){
+                console.log(suite);
+                if(suite.name.indexOf(suiteName) > -1 && suite.name.indexOf(timeStamp) > -1){
+                $scope.suiteToReturn=suite;
+                console.log($scope.suiteToReturn);
+                }
+
+            };
+    return suiteToReturn;
+    }
+
     }]);
