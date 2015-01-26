f78c7fa8ee4aba7d3eb9e24b7225250c72b2ceff
MatteK <mattek@outlook.com>
2015-01-26 09:29:22 +0100

    Updated logs controller

--- src/main/resources/static/scripts/controllers/logs-controller.js
+++ src/main/resources/static/scripts/controllers/logs-controller.js
@@ -1,5 +1,15 @@
 angular.module('webLog')
-    .controller('LogsCtrl',['$scope', function($scope){
+    .controller('LogsCtrl',['$scope', '$http', function($scope, $http){
     $scope.message = "Logs Controller";
     $scope.errorReport={};
+    $scope.log = {};
+    $http.get('/api/log/getloglist')
+    .success(function(data, status, headers, config){ 
+    	if(data){
+    		$scope.log = data;
+    		console.log(data);
+    	}
+    }).error(function(data, status, headers, config){
+    	console.log(data);
+    });
     }]);
