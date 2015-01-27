e6a067fc87abbe543b19986d759abd5b7537314f
Gustav Holfve <gustav.holfve@chas.se>
2015-01-27 16:09:35 +0100

    Tested to get columnnames and print in a loop

--- src/test/java/se/redmind/rmtest/db/test/ReadFromDBTest.java
+++ src/test/java/se/redmind/rmtest/db/test/ReadFromDBTest.java
@@ -1,12 +1,17 @@
 package se.redmind.rmtest.db.test;
 
 import org.junit.Test;
+import org.junit.experimental.results.ResultMatchers;
+
 import se.redmind.rmtest.db.DBCon;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by johan on 15-01-27.
@@ -22,6 +27,7 @@ public class ReadFromDBTest {
             stat = conn.createStatement();
             ResultSet rs = stat.executeQuery("select * from testcases;");
             while(rs.next()){
+            	
                 System.out.println("testcase name = " + rs.getString("name"));
                 System.out.println("testcase driver = " + rs.getString("driver"));
                 System.out.println("testcase error = " + rs.getString("error"));
@@ -45,16 +51,26 @@ public class ReadFromDBTest {
         try {
             stat = conn.createStatement();
             ResultSet rs = stat.executeQuery("select * from reports;");
+            ResultSetMetaData meta = rs.getMetaData();
+            int columnCount = meta.getColumnCount();
+            List<String> columnNames = new ArrayList<String>();
+            for (int i = 1; i <= columnCount; i++) {
+            	columnNames.add(meta.getColumnLabel(i));
+			}
             while (rs.next()) {
-
-                System.out.println("Report name = " + rs.getString("name"));
-                System.out.println("Report suitename = " + rs.getString("suitename"));
-                System.out.println("Report timestamp = " + rs.getString("timestamp"));
-                System.out.println("Report tests = " + rs.getString("tests"));
-                System.out.println("Report skipped = " + rs.getString("skipped"));
-                System.out.println("Report failures = " + rs.getString("failures"));
-                System.out.println("Report time = " + rs.getString("time"));
-                System.out.println("Report id = " + rs.getString("id"));
+            	
+            	for (int i = 1; i < columnNames.size(); i++) {
+            		System.out.println(columnNames.get(i)+": "+rs.getObject(i));
+				}
+            	
+//                System.out.println("Report name = " + rs.getString("name"));
+//                System.out.println("Report suitename = " + rs.getString("suitename"));
+//                System.out.println("Report timestamp = " + rs.getString("timestamp"));
+//                System.out.println("Report tests = " + rs.getString("tests"));
+//                System.out.println("Report skipped = " + rs.getString("skipped"));
+//                System.out.println("Report failures = " + rs.getString("failures"));
+//                System.out.println("Report time = " + rs.getString("time"));
+//                System.out.println("Report id = " + rs.getString("id"));
 
                 rs.close();
             }
