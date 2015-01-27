3499e5af64e843af8524e314939eb3b071416f3a
SnytaxError <grimlund@gmail.comgit config --global user.name SnytaxErrorgit commit -am hello>
2015-01-27 14:06:07 +0100

    database commit batch tests

--- src/main/java/se/redmind/rmtest/db/CommitToDB.java
+++ src/main/java/se/redmind/rmtest/db/CommitToDB.java
@@ -8,10 +8,14 @@ import java.sql.SQLException;
  * Created by johan on 15-01-27.
  */
 public class CommitToDB {
-
         Connection conn;
 
+    public CommitToDB(Connection connection){
+        conn = connection;
+    }
+
     public void commitReport(PreparedStatement prep){
+
 
         try {
             conn.setAutoCommit(false);
--- src/main/java/se/redmind/rmtest/db/ReadFromDB.java
+++ src/main/java/se/redmind/rmtest/db/ReadFromDB.java
@@ -1,7 +1,39 @@
 package se.redmind.rmtest.db;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Created by johan on 15-01-26.
  */
 public class ReadFromDB {
+
+    Connection conn;
+
+    public void readReport(){
+
+        Statement stat = null;
+        try {
+            stat = conn.createStatement();
+            ResultSet rs = stat.executeQuery("select * from reports;");
+        while (rs.next()) {
+
+            System.out.println("Report name = " + rs.getString("name"));
+            System.out.println("Report suitename = " + rs.getString("suitename"));
+            System.out.println("Report timestamp = " + rs.getString("timestamp"));
+            System.out.println("Report tests = " + rs.getString("tests"));
+            System.out.println("Report skipped = " + rs.getString("skipped"));
+            System.out.println("Report failures = " + rs.getString("failures"));
+            System.out.println("Report time = " + rs.getString("time"));
+            System.out.println("Report id = " + rs.getString("id"));
+
+            rs.close();
+        }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
