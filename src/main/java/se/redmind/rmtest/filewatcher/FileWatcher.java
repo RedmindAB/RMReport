090561265a2330ffd2c0a3322a1d806f45150575
Gustav Holfve <gustav.holfve@chas.se>
2015-01-27 14:44:20 +0100

    File watcher finding files

--- src/main/java/se/redmind/rmtest/filewatcher/FileWatcher.java
+++ src/main/java/se/redmind/rmtest/filewatcher/FileWatcher.java
@@ -1,8 +1,35 @@
 package se.redmind.rmtest.filewatcher;
 
+import java.io.IOException;
+import java.nio.channels.UnsupportedAddressTypeException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.WatchService;
+
+import static java.nio.file.StandardWatchEventKinds.*;
 import se.redmind.rmtest.report.reportloader.ReportLoader;
 
 public class FileWatcher {
 
-	public static String directoryPath = "";
+	public static String directoryPath = new ReportLoader(System.getProperty("user.dir")+"/testfiles", false).getReportFolderPath();
+	
+	public static void Run(){
+		System.out.println("File watcher starting to check: "+directoryPath);
+		Path path = Paths.get(directoryPath);
+		if (path == null) {
+			throw new UnsupportedOperationException();
+		}
+		
+		try {
+			WatchService watcher = path.getFileSystem().newWatchService();
+			FileWatcherQueueReader queueReader = new FileWatcherQueueReader(watcher);
+			Thread thread = new Thread(queueReader, "DirectoryWatcher");
+			thread.start();
+			
+			path.register(watcher, ENTRY_CREATE, ENTRY_MODIFY);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- src/main/java/se/redmind/rmtest/filewatcher/FileWatcherQueueReader.java
+++ src/main/java/se/redmind/rmtest/filewatcher/FileWatcherQueueReader.java
@@ -1,11 +1,36 @@
 package se.redmind.rmtest.filewatcher;
 
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+
 public class FileWatcherQueueReader implements Runnable {
 
+	private WatchService watchService;
+	public FileWatcherQueueReader(WatchService watchService) {
+		this.watchService = watchService;
+	}
+	
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Running file watcher");
+		try {
+            // get the first event before looping
+            WatchKey key = watchService.take();
+            while(key != null) {
+                // we have a polled event, now we traverse it and 
+                // receive all the states from it
+                for (WatchEvent event : key.pollEvents()) {
+                    System.out.printf("Received %s event for file: %s\n",
+                                      event.kind(), event.context() );
+                }
+                key.reset();
+                key = watchService.take();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Stopping thread");
 	}
 
 }
