47e95d3382767dd10914c8f239714dbe463d35b5
SnytaxError <grimlund@gmail.comgit config --global user.name SnytaxErrorgit commit -am hello>
2015-01-29 15:44:54 +0100

    more queries

--- src/main/java/se/redmind/rmtest/db/read/ReadTestrunsFromDB.java
+++ src/main/java/se/redmind/rmtest/db/read/ReadTestrunsFromDB.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashSet;
 
 /**
  * Created by johan on 15-01-29.
@@ -11,6 +12,9 @@ import java.sql.Statement;
 public class ReadTestrunsFromDB {
 
     public static Connection conn;
+
+    String GET_ALL_TESTRUNS = "select * from testruns";
+    String GET_ALL_TESTRUNS_BY_NAME = "select * from testruns";
 
     public ReadTestrunsFromDB(Connection connection){
         conn=connection;
@@ -23,6 +27,18 @@ public class ReadTestrunsFromDB {
             return stat.executeQuery(query);
         } catch (SQLException e) {
             // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public HashSet getAllTestruns(){
+        HashSet hs = new HashSet();
+        ResultSet rs = getResulSet(GET_ALL_TESTRUNS);
+        try {
+            while(rs.next())
+                hs.add(rs.getString(1));
+            return hs;
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
