3f383838f4ed27e5b0bc018d214b9ade6f325515
Bambuh <lukas@klingberg.se>
2015-01-29 12:45:29 +0100

    color for charts

--- src/main/resources/static/scripts/controllers/home-controller.js
+++ src/main/resources/static/scripts/controllers/home-controller.js
@@ -20,8 +20,24 @@ angular.module('webLog')
 			       [65, 59, 80, 81, 56],
 			       [28, 48, 40, 19, 86]
 			       ],
-    		color: '#FFFFFF'
-    
+			       colours: [
+			                  { // grey
+			                    fillColor: "rgba(148,159,177,0.2)",
+			                    strokeColor: "rgba(148,159,177,1)",
+			                    pointColor: "rgba(148,159,177,1)",
+			                    pointStrokeColor: "#fff",
+			                    pointHighlightFill: "#fff",
+			                    pointHighlightStroke: "rgba(148,159,177,0.8)"
+			                  },
+			                  { // dark grey
+			                    fillColor: "rgba(77,83,96,0.2)",
+			                    strokeColor: "rgba(77,83,96,1)",
+			                    pointColor: "rgba(77,83,96,1)",
+			                    pointStrokeColor: "#fff",
+			                    pointHighlightFill: "#fff",
+			                    pointHighlightStroke: "rgba(77,83,96,1)"
+			                  }
+			                ]
     				},
     	projectTwo: {
 			labels: ['AftonBladetTV 1', 'AftonBladetTV 2', 
