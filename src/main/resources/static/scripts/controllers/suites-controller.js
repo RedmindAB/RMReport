3da1463598d3c0beb73ab6e7b179eb460f257645
Bambuh <lukas@klingberg.se>
2015-01-30 09:37:46 +0100

    semi working test cases

--- src/main/resources/static/scripts/controllers/suites-controller.js
+++ src/main/resources/static/scripts/controllers/suites-controller.js
@@ -1,10 +1,15 @@
 angular.module('webLog')
 	.controller('SuitesCtrl',[ '$scope', '$location', function($scope, $location){
 
-		$scope.openSuite;
+		$scope.suiteTests;
+		$scope.runRightNow;
+		
+		$scope.testClasses = [ "se.redmind.rmtest.selenium.example.CreateLogsTest(20150121-160622)",
+		                       "se.redmind.rmtest.selenium.example.CreateLogsTestSecond(20150121-160622)"
+		                      ];
+		
 		
 		var statHolder= {};
-		
 		function readDataToCharts(suite){
 			getDriverNames(suite);
 			getTestStats(suite);
@@ -17,22 +22,33 @@ angular.module('webLog')
 			statHolder.drivers = suite.drivers;
 		};
 		
-		function getTestStats(suite){
-			statHolder.testStat = [suite.errors, suite.failures];
-			statHolder.testStat[2] = (suite.tests - (suite.errors + suite.failures));
+//		function getTestStats(suite){
+//			statHolder.testStat = [suite.errors, suite.failures];
+//			statHolder.testStat[2] = (suite.tests - (suite.errors + suite.failures));
+//		}
+		
+	    $scope.goToTestCases = function(currentRun){
+	    	console.log("in suite controller: " + currentRun);
+	    	$scope.runRightNow = currentRun;
+	    	$location.path('/test-case');
+	    	console.log($scope.runRightNow);
+	    };
+		
+		$scope.goToSuiteClasses = function(){
+			$location.path('/suite-run-classes');
 		}
 		
 		$scope.goToSuiteRuns = function(){
 			$location.path('/test-suite-runs');
 		}
 		
-		$scope.$watch('suites', function(suites){
-		    angular.forEach(suites, function(test, idx){
-		      if (test.open) {
-		    	  $scope.openSuite = test;
-		    	  readDataToCharts($scope.openSuite);
-		      }
-		    })   
-		}, true);
+//		$scope.$watch('suites', function(suites){
+//		    angular.forEach(suites, function(test, idx){
+//		      if (test.open) {
+//		    	  $scope.openSuite = test;
+//		    	  readDataToCharts($scope.openSuite);
+//		      }
+//		    })   
+//		}, true);
 		  
 	}]);
