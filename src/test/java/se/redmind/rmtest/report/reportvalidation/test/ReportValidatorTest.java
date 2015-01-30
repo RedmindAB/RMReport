df75eb0ad37166b008c6285d70fe4ae4dc09601a
Gustav Holfve <gustav.holfve@chas.se>
2015-01-30 10:23:34 +0100

    report inserted almost done

---
+++ src/test/java/se/redmind/rmtest/report/reportvalidation/test/ReportValidatorTest.java
@@ -0,0 +1,23 @@
+package se.redmind.rmtest.report.reportvalidation.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import se.redmind.rmtest.report.reportvalidation.ReportValidator;
+
+public class ReportValidatorTest {
+ 	private static String filename = "TEST-test.java.se.redmind.rmtest.selenium.example.CreateLogTests-20150121-160622.xml";
+
+	
+	@Test
+	public void insertReport() {
+		ReportValidator reportValidator = new ReportValidator(filename);
+		boolean exists = reportValidator.reportExists();
+		if (!exists) {
+			reportValidator.saveReport();
+		}
+		
+	}
+
+}
