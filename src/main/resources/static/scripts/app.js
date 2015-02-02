65aaeb8c4fa9a1420aa82ca3909216f0420cdf03
Bambuh <lukas@klingberg.se>
2015-02-02 15:42:13 +0100

    stuffz...

--- src/main/resources/static/scripts/app.js
+++ src/main/resources/static/scripts/app.js
@@ -23,27 +23,9 @@ angular.module('webLog', ['ui.router', 'chart.js','ui.bootstrap','n3-line-chart'
 	        url:'/methods',
 	        templateUrl: 'partials/methods.html',
 	        controller: 'SuitesCtrl'
-        })
-        .state('testCases',{
-	        url:'/test-case',
-	        templateUrl: 'partials/test-case.html',
-	        controller: 'SuitesCtrl'
-        })     
-        .state('suiteRunClasses',{
-	        url:'/suite-run-classes',
-	        templateUrl: 'partials/suite-run-classes.html',
-	        controller: 'SuitesCtrl'
-        })
-        .state('project',{
-	        url:'/project',
-	        templateUrl: 'partials/project.html',
-	        controller: 'ProjectCtrl'
-        })
-        .state('suiteRuns',{
-	        url:'/test-suite-runs',
-	        templateUrl: 'partials/test-suite-runs.html',
-	        controller: 'SuitesCtrl'
         });
+
+    
     
     });
 
