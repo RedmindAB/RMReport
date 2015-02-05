c9b04528ac6967cf06783f9ffcf8e372fa6e9577
Gustav Holfve <gustav.holfve@chas.se>
2015-02-05 14:10:33 +0100

    removed test files

--- src/main/java/se/redmind/rmtest/db/InMemoryDBHandler.java
+++ src/main/java/se/redmind/rmtest/db/InMemoryDBHandler.java
@@ -1,6 +1,7 @@
 package se.redmind.rmtest.db;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -11,8 +12,18 @@ public class InMemoryDBHandler {
 	public void init(){
 		try {
 			connection.setAutoCommit(false);
+			String databasePath = System.getProperty("user.dir")+"/RMTest.db";
+			System.out.println(databasePath);
 			Statement initStatement = connection.createStatement();
-			initStatement.addBatch("ATTACH");
+			initStatement.addBatch("ATTACH DATABASE "+databasePath+" AS rmtest;");
+			initStatement.addBatch("INSERT INTO report FROM SELECT * FROM rmtest.report WHERE timestamp >= (SELECT MIN(timestamp) FROM (SELECT DISTINCT timestamp FROM report WHERE suite_id = 1 ORDER BY timestamp DESC LIMIT 50));");
+			initStatement.executeBatch();
+			connection.setAutoCommit(true);
+//			Statement createStatement = connection.createStatement();
+//			ResultSet executeQuery = createStatement.executeQuery("SELECT * FROM report;");
+//			while (executeQuery.next()) {
+//				System.out.println(executeQuery.getString(1));
+//			}
 			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
---
+++ src/test/java/se/redmind/rmtest/db/inmemory/test/InitInmemoryTest.java
@@ -0,0 +1,16 @@
+package se.redmind.rmtest.db.inmemory.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import se.redmind.rmtest.db.InMemoryDBHandler;
+
+public class InitInmemoryTest {
+
+	@Test
+	public void test() {
+		new InMemoryDBHandler().init();
+	}
+
+}
