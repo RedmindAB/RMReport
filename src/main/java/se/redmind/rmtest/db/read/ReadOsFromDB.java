f1a6e37a4713284a48dc514b05cc1d22c25b4052
SnytaxError <grimlund@gmail.comgit config --global user.name SnytaxErrorgit commit -am hello>
2015-02-12 15:26:54 +0100

    getOsVersionAndId

---
+++ src/main/java/se/redmind/rmtest/db/read/ReadOsFromDB.java
@@ -0,0 +1,31 @@
+package se.redmind.rmtest.db.read;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+
+import se.redmind.rmtest.db.DBBridge;
+
+public class ReadOsFromDB extends DBBridge {
+	
+	String GET_ALL_FROM_OS = "select * from os";
+	
+	public HashMap<String, String> getOsVersionAndId(){
+		HashMap<String, String> hm = new HashMap();
+		ResultSet rs = readFromDB(GET_ALL_FROM_OS);
+		
+		try {
+			while(rs.next()){
+				hm.put("os_id", rs.getString("os_id"));
+				hm.put("OSversion", rs.getString("name")+rs.getString("version"));
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return hm;
+	}
+	
+}
