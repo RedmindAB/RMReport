4bcfe2e587e4966a4dfd2cb5027bfa8dc4e74e9e
SnytaxError <grimlund@gmail.comgit config --global user.name SnytaxErrorgit commit -am hello>
2015-02-13 08:30:18 +0100

    get all from device and browser

---
+++ src/main/java/se/redmind/rmtest/db/read/ReadBrowserFromDB.java
@@ -0,0 +1,28 @@
+package se.redmind.rmtest.db.read;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+
+import se.redmind.rmtest.db.DBBridge;
+
+public class ReadBrowserFromDB extends DBBridge{
+
+String GET_ALL_FROM_BROWSER = "select * from browser";
+	
+
+	public HashMap<String, Integer> getBrowserVersionAndId(){
+		HashMap<String, Integer> hm = new HashMap<String, Integer>();
+		ResultSet rs = readFromDB(GET_ALL_FROM_BROWSER);
+		
+		try {
+			while(rs.next()){
+				hm.put(rs.getString("name")+rs.getString("version"), rs.getInt("browser_id"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return hm;
+	}
+}
---
+++ src/main/java/se/redmind/rmtest/db/read/ReadDeviceFromDB.java
@@ -0,0 +1,30 @@
+package se.redmind.rmtest.db.read;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+
+import se.redmind.rmtest.db.DBBridge;
+
+public class ReadDeviceFromDB extends DBBridge {
+	
+	String GET_ALL_FROM_DEVICE = "select * from device";
+	
+
+	public HashMap<String, Integer> getDeviceNameAndId(){
+		HashMap<String, Integer> hm = new HashMap<String, Integer>();
+		ResultSet rs = readFromDB(GET_ALL_FROM_DEVICE);
+		
+		try {
+			while(rs.next()){
+				hm.put(rs.getString("name"), rs.getInt("device_id"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return hm;
+	}
+	
+
+}
