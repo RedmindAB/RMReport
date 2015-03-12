2eb8feab331b9153dbf43a0fcc24ea44b7e7ff02
MatteK <mattek@outlook.com>
2015-03-12 16:50:46 +0100

    Added modal and slider

--- src/main/resources/static/scripts/controllers/main-controller.js
+++ src/main/resources/static/scripts/controllers/main-controller.js
@@ -1,6 +1,34 @@
 angular.module('webLog')
     .controller('MainCtrl',['$scope', '$rootScope', '$http','$location', '$timeout','$state', 'CurrentSuite', 'Charts', 'Utilities', function($scope, $rootScope,$http, $location, $timeout, $state, CurrentSuite, Charts, Utilities){
-    	
+        $scope.slides = [
+                         {image: 'img/img00.jpg', description: 'Image 00'},
+                         {image: 'img/img01.jpg', description: 'Image 01'},
+                         {image: 'img/img02.jpg', description: 'Image 02'},
+                         {image: 'img/img03.jpg', description: 'Image 03'},
+                         {image: 'img/img04.jpg', description: 'Image 04'}
+                     ];
+
+                     $scope.direction = 'left';
+                     $scope.currentIndex = 0;
+
+                     $scope.setCurrentSlideIndex = function (index) {
+                         $scope.direction = (index > $scope.currentIndex) ? 'left' : 'right';
+                         $scope.currentIndex = index;
+                     };
+
+                     $scope.isCurrentSlideIndex = function (index) {
+                         return $scope.currentIndex === index;
+                     };
+
+                     $scope.prevSlide = function () {
+                         $scope.direction = 'left';
+                         $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
+                     };
+
+                     $scope.nextSlide = function () {
+                         $scope.direction = 'right';
+                         $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
+                     };
     $scope.Charts = Charts;
     $scope.CurrentSuite = CurrentSuite;
     $scope.Utilities = Utilities;
@@ -1455,4 +1483,39 @@ angular.module('webLog')
     	chart.title.text = "Percentage of passed tests";
     	delete Charts.mainChart.options.tooltip.valueDecimals;
 	}
-}]);
+}])
+.animation('.slide-animation', function () {
+        return {
+            beforeAddClass: function (element, className, done) {
+                var scope = element.scope();
+
+                if (className == 'ng-hide') {
+                    var finishPoint = element.parent().width();
+                    if(scope.direction !== 'right') {
+                        finishPoint = -finishPoint;
+                    }
+                    TweenMax.to(element, 0.5, {left: finishPoint, onComplete: done });
+                }
+                else {
+                    done();
+                }
+            },
+            removeClass: function (element, className, done) {
+                var scope = element.scope();
+
+                if (className == 'ng-hide') {
+                    element.removeClass('ng-hide');
+
+                    var startPoint = element.parent().width();
+                    if(scope.direction === 'right') {
+                        startPoint = -startPoint;
+                    }
+
+                    TweenMax.fromTo(element, 0.5, { left: startPoint }, {left: 0, onComplete: done });
+                }
+                else {
+                    done();
+                }
+            }
+        };
+    });
