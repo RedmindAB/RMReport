d72701c9384aec7ea67426f25b7b58b3fa75b92b
MatteK <mattek@outlook.com>
2015-03-13 17:00:02 +0100

    modalShown changes

--- src/main/resources/static/scripts/controllers/main-controller.js
+++ src/main/resources/static/scripts/controllers/main-controller.js
@@ -1,6 +1,23 @@
 angular.module('webLog')
     .controller('MainCtrl',['$scope', '$rootScope', '$http','$location', '$timeout','$state', 'CurrentSuite', 'Charts', 'Utilities','ScreenshotMaster', function($scope, $rootScope,$http, $location, $timeout, $state, CurrentSuite, Charts, Utilities,ScreenshotMaster){
-    	$scope.screenshotTestArray = ["img/img00.jpg", "img/img01.jpg"];
+    	
+    		  $scope.modalShown = false;
+    		  $scope.toggleModal = function() {
+    			  
+    			  if($scope.modalShown != true){
+    				     window.setTimeout(function() {
+    				    	 $scope.modalShown = true;
+    				    	 console.log("modalShown Before: " + $scope.modalShown);
+    				        }, 1000);
+    			  }
+    			  else if($scope.modalShown != false){
+ 				     window.setTimeout(function() {
+				    	 $scope.modalShown = false;
+				    	 console.log("modalShown After: " + $scope.modalShown);
+				        }, 1000);
+    			  }
+    		  };
+    	$scope.screenshotTestArray = ["img/img00.jpg", "img/img01.jpg", "img/AnotherGeneratedSuite.png"];
         $scope.slides = [
                          {image: 'img/img00.jpg', description: 'Image 00'},
                          {image: 'img/img01.jpg', description: 'Image 01'},
