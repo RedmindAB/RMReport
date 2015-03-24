695e1d92a04369db62f4e1ca122ae7b7557a2f75
Lukas Klingberg <lukas@klingberg.se>
2015-03-24 08:13:56 +0100

    added correct width to screenshot divs

--- src/main/resources/static/scripts/controllers/screenshot-controller.js
+++ src/main/resources/static/scripts/controllers/screenshot-controller.js
@@ -5,7 +5,7 @@ angular.module('webLog').controller('ScreenshotCtrl', ['$window', '$scope', '$ro
 	$scope.caseArraySize = []
 	
 	$scope.getMethodContentWidth = function(method){
-		return (method.testcases.length * 252)+10;
+		return (method.testcases.length * 232)+10;
 	}
 	
 	$scope.toggleModal = function() {
