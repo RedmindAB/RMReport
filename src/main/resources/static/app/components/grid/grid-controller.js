59d0f1030dc7f85ca1376dc1adf811f9e6507059
Lukas Klingberg <lukas@klingberg.se>
2015-04-15 10:14:02 +0200

    mock data for grid view

--- src/main/resources/static/app/components/grid/grid-controller.js
+++ src/main/resources/static/app/components/grid/grid-controller.js
@@ -2,6 +2,9 @@ angular.module('webLog')
 .controller("GridCtrl", ["$scope", "$http", function($scope, $http){
 	
 	$scope.gridData = undefined;
+	$scope.mockData ={
+			FreeProxies:[]
+	};
 	
 	$scope.isDesktop = function(dataObj){
 		return dataObj === "MAC" || dataObj === "PC";
@@ -36,4 +39,139 @@ angular.module('webLog')
     }).error(function(data, status, headers, config){
     	console.error(data);
     });
+	
+	mockStuff();
+	
+	function mockStuff(){
+		$scope.mockData.FreeProxies = [];
+		
+		for (var i = 0; i < 50; i++) {
+			if (i < 10) {
+				
+				var data = {
+						configuration:{
+							port: 6651
+						},
+						capabilities: [
+						               {
+						            	   seleniumProtocol: "WebDriver",
+						            	   osname: "OSX",
+						            	   platform: "MAC",
+						            	   browserName: "firefox",
+						            	   
+						               },
+						               {
+						   				seleniumProtocol: "WebDriver",
+										osname: "OSX",
+										platform: "MAC",
+										browserName: "chrome",
+						               }
+						               ]
+				}
+				
+				$scope.mockData.FreeProxies.push(data);
+			}
+			
+			if (i > 10 && i < 20) {
+				
+				var data = {
+						configuration:{
+							port: 6652
+						},
+						capabilities: [
+						               {
+						            	   seleniumProtocol: "WebDriver",
+						            	   osname: "WINDOWS",
+						            	   platform: "PC",
+						            	   browserName: "firefox",
+						            	   
+						               },
+						               {
+						   				seleniumProtocol: "WebDriver",
+										osname: "WINDOWS",
+										platform: "PC",
+										browserName: "chrome",
+						               }
+						               ]
+				}
+				
+				$scope.mockData.FreeProxies.push(data);
+			}
+			
+			if (i > 20 && i < 30) {
+				
+				var data = {
+						configuration:{
+							port: 6653
+						},
+						capabilities: [
+						               {
+						            	    rmDeviceType: "mobile",
+						               		osname: "ANDROID",
+											platform: "ANDROID",
+											platformName: "Android",
+											appActivity: ".debug.DebugMainActivity",
+											appWaitActivity: ".Splash",
+											platformVersion: "5.1",
+											deviceName: "Nexus 6",
+											deviceId: "ZX1G524BJK",
+						               }
+						               ]
+				}
+				
+				$scope.mockData.FreeProxies.push(data);
+				
+				
+			}
+			
+			if (i > 30 && i < 40) {
+				var data = {
+						configuration:{
+							port: 6654
+						},
+						capabilities: [
+						               {
+						            	    rmDeviceType: "mobile",
+						               		osname: "IOS",
+											platform: "IOS",
+											platformName: "IOS",
+											appActivity: ".debug.DebugMainActivity",
+											appWaitActivity: ".Splash",
+											platformVersion: "11",
+											deviceName: "iPhone 6",
+											deviceId: "ZX1G524BJK",
+						               }
+						               ]
+				}
+				
+				$scope.mockData.FreeProxies.push(data);
+			}
+			
+			if (i > 40 && i < 50) {
+				
+				var data = {
+						configuration:{
+							port: 6653
+						},
+						capabilities: [
+						               {
+						            	    rmDeviceType: "Surf Pad",
+						               		osname: "IOS",
+											platform: "IOS",
+											platformName: "IOS",
+											appActivity: ".debug.DebugMainActivity",
+											appWaitActivity: ".Splash",
+											platformVersion: "11",
+											deviceName: "iPa",
+											deviceId: "ZX1G524BJK",
+						               }
+						               ]
+				}
+				
+				$scope.mockData.FreeProxies.push(data);
+			}
+		}
+		console.log($scope.mockData);
+	}
+	
 }]);
