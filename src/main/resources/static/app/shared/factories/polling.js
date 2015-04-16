f10c7f7ebe8f1c87eadac02a4399683d0c83ef98
Lukas Klingberg <lukas@klingberg.se>
2015-04-16 16:19:48 +0200

    added last file

---
+++ src/main/resources/static/app/shared/factories/polling.js
@@ -0,0 +1,29 @@
+angular.module('webLog')
+.factory('Polling', ['$http', function($http) {
+	var defaultPollingTime = 5000;
+	var polls = {};
+	return {
+		startPolling: function(name, url, dataObj,func, pollingTime){
+			if(!polls[name]){
+				var poller = function(){
+					$http.get(url)
+				    .success(function(data, status, headers, config){ 
+				    	if(data){
+				    		if (dataObj !== data) {
+				    			func(data);
+							}
+				    	};
+				    }).error(function(data, status, headers, config){
+				    	console.error(data);
+				    });
+				}
+				poller();
+				polls[name] = setInterval(poller, pollingTime || defaultPollingTime);
+			}
+		},
+		stopPolling: function(name){
+			clearInterval(polls[name]);
+			delete polls[name];
+		}
+	};
+}]);
