9c2427088429a68caaacf59fd9e07667654f3915
SnytaxError <grimlund@gmail.comgit config --global user.name SnytaxErrorgit commit -am hello>
2015-04-22 10:17:51 +0200

    Test for SeleniumGridDao

---
+++ src/test/java/se/redmind/rmtest/webservicetests/SeleniumGridDAOTest.java
@@ -0,0 +1,23 @@
+package se.redmind.rmtest.webservicetests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import se.redmind.rmtest.web.route.api.seleniumgrid.SeleniumGridDAO;
+import se.redmind.rmtest.ws.testsuite.WSSetupHelper;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+public class SeleniumGridDAOTest extends WSSetupHelper{
+
+	
+	@Test
+	public void getSeleniumGridTest()  {
+		SeleniumGridDAO sgDao = new SeleniumGridDAO();
+		String result = sgDao.getSelenumGridData();
+		JsonObject expected = new Gson().fromJson(result, JsonObject.class);
+		
+		assertTrue(expected.isJsonObject());
+
+	}
+}
