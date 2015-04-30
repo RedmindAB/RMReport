91b49d106137bd4f05e76ba0667d2e3baa28efda
Gustav Holfve <gustav.holfve@chas.se>
2015-04-30 09:30:34 +0200

    Ignored selenium test because its dependent on external files that are not created for the test

--- src/test/java/se/redmind/rmtest/webservicetests/SeleniumGridDAOTest.java
+++ src/test/java/se/redmind/rmtest/webservicetests/SeleniumGridDAOTest.java
@@ -1,16 +1,20 @@
 package se.redmind.rmtest.webservicetests;
 
 import static org.junit.Assert.*;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
 import se.redmind.rmtest.web.route.api.seleniumgrid.SeleniumGridDAO;
 import se.redmind.rmtest.ws.testsuite.WSSetupHelper;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
 public class SeleniumGridDAOTest extends WSSetupHelper{
 
 	
+	@Ignore
 	@Test
 	public void getSeleniumGridTest()  {
 		SeleniumGridDAO sgDao = new SeleniumGridDAO();
