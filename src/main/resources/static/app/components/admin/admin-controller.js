43bc452020e0a1391d4d43085080fa8d906aae4e
Lukas Klingberg <lukas@klingberg.se>
2015-05-18 16:12:35 +0200

    started admin.html

--- src/main/resources/static/app/components/admin/admin-controller.js
+++ src/main/resources/static/app/components/admin/admin-controller.js
@@ -1,4 +1,29 @@
 angular.module('webLog')
 .controller('AdminCtrl',['$scope', '$http','$state', function($scope, $http,$state){
 
+	$scope.config = {};
+	$scope.newPath = '';
+	
+	$scope.addPath = function(path){
+		console.log("adding path");
+	}
+	
+	$scope.removePath = function(index){
+		console.log("removing path: " + index);
+	}
+	
+	$scope.changePath = function(path, index){
+		console.log("changing path: " + index);
+	}
+	
+    $http.get('/api/admin/config')
+    .success(function(data, status, headers, config){ 
+    	if(data){
+    		$scope.config = data;
+    		console.log($scope.config);
+    	};
+    }).error(function(data, status, headers, config){
+    	console.error(data);
+    });
+	
 }]);
