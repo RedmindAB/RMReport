633fe0bc47977014a98f99a27d127326e73b419f
Lukas Klingberg <lukas@klingberg.se>
2015-06-11 15:50:30 +0200

    sorted up navbar-controller

--- src/main/resources/static/app/components/navbar/navbar-controller.js
+++ src/main/resources/static/app/components/navbar/navbar-controller.js
@@ -10,46 +10,49 @@
 	function NavBarCtrl ($state,$window, ChartMaker, RestLoader, Utilities, CurrentSuite, DashboardServices) {
 	
 		var vm = this;
+		
 		var count = 0;
+		
 		vm.CurrentSuite = CurrentSuite;
 		
+		vm.checkPosition					= checkPosition;
+		vm.chooseProject					= chooseProject;
+		vm.getScreenshotsFromTimestamp		= getScreenshotsFromTimestamp;
+		vm.getSuiteSkeletonByTimestamp		= getSuiteSkeletonByTimestamp;
+		vm.getSuiteSkeleton					= getSuiteSkeleton;
+		vm.highlightPoint					= highlightPoint;
+		vm.isSmallWindowToggle 				= isSmallWindowToggle;
+		vm.isSmallWindowTarget				= isSmallWindowTarget;
+		vm.loadDashboardPlatformsAndDevices = loadDashboardPlatformsAndDevices;
+		vm.loadDashboardDeviceRange 		= loadDashboardDeviceRange;
+		vm.loadMainChart					= loadMainChart;
 		
-		vm.loadDashboardPlatformsAndDevices = function(){
-			DashboardServices.getPlatforms(CurrentSuite.currentSuiteInfo.id, "android").then(function(){
-				DashboardServices.getDevices(CurrentSuite.currentSuiteInfo.id).then(function(){
-					DashboardServices.getClasses(CurrentSuite.currentSuiteInfo.id);	
-				});
-			});
-			vm.loadDashboardDeviceRange();
-		};
-		
-		vm.loadDashboardDeviceRange = function(){
-			DashboardServices.getDeviceRange(CurrentSuite.currentSuiteInfo.id, CurrentSuite.currentSuiteInfo.lastTimeStamp);
-		};
-		
-		vm.isSmallWindowToggle = function(){
-			return $window.window.innerWidth < 900  ? 'collapse' : '';
-		};
-		
-		vm.isSmallWindowTarget = function(){
-			return $window.width < 900  ? '.navbar-collapse' : '';
-		};
-		
-	    vm.highlightPoint = function(timestamp){
-	    	ChartMaker.highlightPoint(Utilities.getIndexByTimestamp(timestamp));
-	    };
+	    function chooseProject(){
+	    	if(CurrentSuite.currentSuiteInfo.name === undefined){
+	    		return 'Choose Project';
+	    	}
+	    	else{
+	    		return CurrentSuite.currentSuiteInfo.name;
+	    	}
+	    }
 	    
-		vm.getSuiteSkeletonByTimestamp = function(timestamp){
-			RestLoader.loadTimestamp(timestamp);
-		};
+	    function checkPosition(){
+	    	if($state.$current.name === "home"){
+	    		$state.transitionTo("dashboard");
+	    	}
+	    }
 		
-		vm.getScreenshotsFromTimestamp = function(){
+		function getSuiteSkeletonByTimestamp(timestamp){
+			RestLoader.loadTimestamp(timestamp);
+		}
+		
+		function getScreenshotsFromTimestamp(){
 			if ($state.$current.name === 'screenshots.methods') {
 				RestLoader.loadScreenshotsFromClass(CurrentSuite.currentClass);
 			}
-		};
+		}
 		
-		vm.getSuiteSkeleton= function(suite){
+		function getSuiteSkeleton(suite){
 			RestLoader.getSuiteSkeleton(suite);
 			if ($state.includes("reports")) {
 				$state.transitionTo("reports.classes");
@@ -57,25 +60,35 @@
 				$state.transitionTo("screenshots.classes");
 			} else {
 			}
-		};
-		
-	    vm.loadMainChart = function(suiteID, newLine, name){
-	    	ChartMaker.loadMainChart(suiteID,newLine, name);
-	    };
-		
-	    vm.chooseProject = function(){
-	    	if(CurrentSuite.currentSuiteInfo.name === undefined){
-	    		return 'Choose Project';
-	    	}
-	    	else{
-	    		return CurrentSuite.currentSuiteInfo.name;
-	    	}
-	    };
+		}
 	    
-	    vm.checkPosition = function(){
-	    	if($state.$current.name === "home"){
-	    		$state.transitionTo("dashboard");
-	    	}
+	    function highlightPoint(timestamp){
+	    	ChartMaker.highlightPoint(Utilities.getIndexByTimestamp(timestamp));
+	    }
+		
+		function isSmallWindowToggle(){
+			return $window.window.innerWidth < 900  ? 'collapse' : '';
+		}
+		
+		function isSmallWindowTarget(){
+			return $window.width < 900  ? '.navbar-collapse' : '';
+		}
+	    
+		function loadDashboardPlatformsAndDevices(){
+			DashboardServices.getPlatforms(CurrentSuite.currentSuiteInfo.id, "android").then(function(){
+				DashboardServices.getDevices(CurrentSuite.currentSuiteInfo.id).then(function(){
+					DashboardServices.getClasses(CurrentSuite.currentSuiteInfo.id);	
+				});
+			});
+			loadDashboardDeviceRange();
+		}
+		
+		function loadDashboardDeviceRange(){
+			DashboardServices.getDeviceRange(CurrentSuite.currentSuiteInfo.id, CurrentSuite.currentSuiteInfo.lastTimeStamp);
+		}
+		
+	    function loadMainChart(suiteID, newLine, name){
+	    	ChartMaker.loadMainChart(suiteID,newLine, name);
 	    }
 	}
 })();
