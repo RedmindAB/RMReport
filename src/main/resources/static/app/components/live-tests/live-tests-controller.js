fac5be2206eb295999b55fea192ac2119e1e604d
MatteK <mattek@outlook.com>
2015-06-15 16:55:36 +0200

    Added content with mocked data to the Live Tests view

--- src/main/resources/static/app/components/live-tests/live-tests-controller.js
+++ src/main/resources/static/app/components/live-tests/live-tests-controller.js
@@ -12,5 +12,14 @@
 		var vm = this;
 		var requestObj = {};
 	
+		vm.mockedPassedTests = ['test_chooseAnotherClass', 'test_Specifications_PlatformVersion', 'test_changeRunLimit_500', 'test_Specifications_CheckDeviceGetPlatform', 'test_ClickOnSuiteLink', 'test_ClearCheckBoxesVersion', 'test_Specifications_Version', 'test_gridGetJson', 'test_Specifications_VersionPlatform', 'test_RunTime'];
+		vm.mockedNewTests = ['test_openSysos', 'test_opencloseSyso', 'test_chooseAnotherClass', 'test_chooseTimestamp', 'test_changeProject', 'test_isThumbnailPresent', 'test_isScreenShotPresent', 'test_isScreenShotSwitched', 'test_goToAdmin', 'test_goToDashboard'];
+		vm.statistics = ['6.74', '144', '65', '0'];
+	
+		runProgressBar();
+		
+		function runProgressBar(){
+			LiveTestsServices.runProgressBar();
+		}
 	}
 })();
--- src/main/resources/static/app/components/live-tests/live-tests-services.js
+++ src/main/resources/static/app/components/live-tests/live-tests-services.js
@@ -9,7 +9,37 @@
 	
 	function LiveTestsServices ($http, $q, CurrentSuite, Utilities) {
 
+		
+		var progressBar = runProgressBar;
+		
 		return {
+			runProgressBar: progressBar
 		};
+		
+		function runProgressBar(){
+			setTimeout(function(){
+			
+			    $('.progress .progress-bar').each(function() {
+			        var me = $(this);
+			        var perc = me.attr("data-percentage");
+			
+			        var current_perc = 0;
+			
+			        var progress = setInterval(function() {
+			            if (current_perc>=perc) {
+			                clearInterval(progress);
+			            } else {
+			                current_perc +=1;
+			                me.css('width', (current_perc)+'%');
+			            }
+			
+			            me.text((current_perc)+'%');
+			
+			        }, 1000);
+			
+			    });
+			
+			},1000);
+		}
 	}
 })();
